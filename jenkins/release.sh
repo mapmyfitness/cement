e1e6abdf81dc17843b1ffc8aae58097268d27223
Kyle Rockman <kyle.rockman@mac.com>
2014-09-26 08:56:53 -0500

    pbr update
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- jenkins/release.sh
+++ jenkins/release.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-venv_name=.virtualenv_release
-virtualenv_activate=./${venv_name}/bin/activate
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/clean.sh
+. $DIR/configure.sh
+. $DIR/virtualenv.sh
 
-# Validate the virtualenv and activate it
-if [[ ! -e $virtualenv_activate ]]
-then
-  virtualenv $venv_name
-fi
-. ${virtualenv_activate}
-
-pip install wheel
-
-if [[ "${RELEASE_TYPE}" == "" ]]
+if [[ "${PBR_RELEASE_TYPE}" == "" ]]
 then 
-  RELEASE_TYPE=patch
+  PBR_RELEASE_TYPE=patch
 fi
 if [[ "${PYPI_NAME}" == "" ]]
 then 
-  PYPI_NAME=mmfpypi
+  PYPI_NAME=pypi
 fi
 
 if [[ -e "./setup.py" ]]
 then
-  python setup.py tag --${RELEASE_TYPE} register -r ${PYPI_NAME} sdist bdist_egg bdist_wheel upload -r ${PYPI_NAME}
+  python setup.py tag --${PBR_RELEASE_TYPE} register -r ${PYPI_NAME} sdist bdist_egg bdist_wheel upload -r ${PYPI_NAME}
 fi
